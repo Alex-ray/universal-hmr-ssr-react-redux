dd6baa2d51b44bcf755d76b19ca80af5395ed0a6
Alex J Ray <alexjamesray@gmail.com>
2016-09-07 15:50:37 -0700

    Adds note about a potential breaking change in react 16

--- src/server/ssr.js
+++ src/server/ssr.js
@@ -9,6 +9,14 @@ function renderApp(res, store, assets, renderProps) {
   const location = renderProps && renderProps.location && renderProps.location.pathname || '/';
   // Needed so some components can render based on location
   store.dispatch(push(location));
+
+  /**
+   * Note: react-dom-strem is calling the PropTypes directly which will cause the code to break in
+   * React 16, waiting on a PR for issue https://github.com/aickin/react-dom-stream/issues/22
+   *
+   * see https://facebook.github.io/react/warnings/dont-call-proptypes.html
+   **/
+
   const htmlStream = renderToStaticMarkup(
     <Html
       title="Super Future"
